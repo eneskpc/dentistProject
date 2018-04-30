27f85c9fa9be24c6548d17274baea33abe7df0ac
Enes Kapucu <enes.kapucu@outlook.com.tr>
2018-05-01 02:40:38 +0300

    Bir çok değişiklik

---
+++ DentistProject/Controllers/HastaTakipController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DentistProject.Controllers
+{
+    public class HastaTakipController : Controller
+    {
+        // GET: HastaTakip
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Giris()
+        {
+            return View();
+        }
+
+        public ActionResult UyeOL()
+        {
+            return View();
+        }
+
+        public ActionResult RandevuAl()
+        {
+            return View();
+        }
+    }
+}
